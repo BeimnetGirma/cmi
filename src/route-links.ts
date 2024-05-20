c67e48c6c6e5917ae4a7235d661123cc13470c0c
rabra <rabira.hierpa@gmail.com>
2024-05-20 08:37:26 +0300

    feat: Add navigation links to home page

---
+++ src/route-links.ts
@@ -0,0 +1,28 @@
+const NavLinks = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Services",
+    href: "/services",
+  },
+  {
+    title: "Contact ",
+    href: "/contact",
+  },
+  {
+    title: "Support",
+    href: "/support",
+  },
+  {
+    title: "News",
+    href: "/news",
+  },
+];
+
+export default NavLinks;
