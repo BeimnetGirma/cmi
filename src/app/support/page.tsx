3dd9957b688d0afce98ffd59afc8f1eab895945d
Beimnet Girma <beimnet.girma@aau.edu.et>
2024-05-21 22:44:20 +0300

    feat: adds contact us page

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -8,7 +8,7 @@ const Support = () => {
       <div className="relative h-80 md:h-80 bg-auto bg-center bg-no-repeat flex items-center justify-center text-white bg-[url('/assets/imgs/header-services.svg')]">
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="text-center z-10 text-sm">
-          <h1 className="text-xl md:text-3xl font-bold">Support</h1>
+          <h1 className="text-xl md:text-3xl font-bold">BEST SOLUTION FOR CONSTRUCTION INDUSTRY</h1>
           <p className="text-sm md:text-lg pt-2">
             {" "}
             <Link href={"/"} className="hover:text-slate-300">
