9801874115b229c90c208fb62895e0ce86941e9b
rabra <rabira.hierpa@gmail.com>
2024-07-01 10:14:17 +0300

    refactor: Update import path for Footer component in not-found.tsx
    The code changes in the not-found.tsx file update the import path for the Footer component. The previous import path was "@/components/footer" and it has been changed to "../../components/footer". This change ensures that the correct Footer component is imported and used in the not-found page.
    
    Recent user commits and repository commits indicate a focus on refactoring, updating dependencies, and updating component imports, which aligns with these changes.

--- src/app/[lng]/not-found.tsx
+++ src/app/[lng]/not-found.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Footer from "@/components/footer";
+import Footer from "../../components/footer";
 
 const Custom404 = () => {
   return (
