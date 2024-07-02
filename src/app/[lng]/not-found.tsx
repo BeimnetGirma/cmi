19d4c4fe3bcaafc02dafc74dc9a41fc66a2e05ff
rabra <rabira.hierpa@gmail.com>
2024-07-02 12:27:39 +0300

    refactor: Update import path for Footer component in not-found.tsx

--- src/app/[lng]/not-found.tsx
+++ src/app/[lng]/not-found.tsx
@@ -1,18 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 import Footer from "../../components/footer";
+import { PageProps } from "@/types";
+import { useTranslation } from "../i18n";
 
-const Custom404 = () => {
+const Custom404: React.FC<PageProps> = async ({ params: { lng } }) => {
+  const { t } = await useTranslation(lng, "translation");
   return (
     <>
       <div className="flex flex-col items-center pt-40">
         <Image src="/assets/imgs/404.png" alt="404" width={600} height={700} />
         <h2 className="text-xl font-bold text-primary-main text-center mt-10 mb-5 p-0">
-          PAGE NOT FOUND
+          {t("notFound")}
         </h2>
-        <p className="text-center text-sm mb-4">
-          The page you&apos; re looking for could not be found.
-        </p>
+        <p className="text-center text-sm mb-4">{t("notFoundContent")}</p>
         <Link
           href={"/"}
           className="bg-primary-main  hover:bg-blue-900 rounded-md transition-colors text-white px-4 py-2 mt-2"
@@ -21,7 +22,7 @@ const Custom404 = () => {
         </Link>
       </div>
 
-      <Footer />
+      <Footer params={{ lng }} />
     </>
   );
 };
