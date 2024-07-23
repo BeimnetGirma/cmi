b839d16b3e8f9e6de744905c8f3361f00a48c9d7
rabra <rabira.hierpa@gmail.com>
2024-07-23 14:26:14 +0300

    refactor: Update Ghost API URL and key in posts.ts

--- src/app/ghost/posts.ts
+++ src/app/ghost/posts.ts
@@ -1,15 +1,19 @@
 import GhostContentAPI from "@tryghost/content-api";
 // Create API instance with site credentials
+
 const api = new GhostContentAPI({
-  url: "http://localhost:2368",
-  key: "916c461c22693aa2431804693c",
+  url: "http://localhost:8005",
+  key: "cb2db959f0ba68cf299a1b6dc7",
   version: "v4.8",
 });
 
 export async function getPosts() {
-  return await api.posts.browse({ limit: "all" }).catch((e) => {
-    console.error(e);
-  });
+  return await api.posts
+    //@ts-expect-error
+    .browse({ include: "tags,authors", limit: "all" })
+    .catch((e) => {
+      console.error(e);
+    });
 }
 export async function getSinglePost(postSlug: string) {
   return await api.posts.read({ slug: postSlug }).catch((e) => {
