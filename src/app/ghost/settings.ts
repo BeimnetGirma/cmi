b84bf894fd413c72d54ce6a5d415b123375416ce
rabra <rabira.hierpa@gmail.com>
2024-09-03 23:53:09 +0300

    refactor: Update Ghost URL in posts.ts and settings.ts

--- src/app/ghost/settings.ts
+++ src/app/ghost/settings.ts
@@ -2,7 +2,7 @@ import GhostContentAPI from "@tryghost/content-api";
 // Create API instance with site credentials
 
 const api = new GhostContentAPI({
-  url: process.env.NEXT_PUBLIC_GHOST_URL as string,
+  url: String(process.env.NEXT_PUBLIC_GHOST_URL).replace("/ghost", ""),
   key: process.env.NEXT_PUBLIC_GHOST_CONTENT_API_KEY as string,
   version: "v4.8",
 });
