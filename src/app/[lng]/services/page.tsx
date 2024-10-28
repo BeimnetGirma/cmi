0cf5f790b3700eca6ebfdd5574c170760f63335e
rabra <rabira.hierpa@gmail.com>
2024-10-28 15:52:44 +0300

    feat: add links to LMS and EMS
    - Refactor background image styling in about, contact, and services pages

--- src/app/[lng]/services/page.tsx
+++ src/app/[lng]/services/page.tsx
@@ -20,297 +20,51 @@ const Services: React.FC<PageProps> = async ({ params: { lng } }) => {
           </p>
         </div>
       </div>
-      <div>
-        <div className="flex flex-col justify-center pt-20">
-          <h1 className="text-primary-main text-xl md:text-xl font-bold text-center">
-            {t("whatWeDo")}
-          </h1>
-          <div className="flex justify-center pt-8">
-            <p className="text-sm text-slate-800 w-1/3 text-center">
-              {t("whatWeDoContent")}
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center p-10 w-1/2 m-auto 2">
-        <div className="grid grid-cols-3 md:grid-cols-3">
-          <div className="p-4 flex flex-col items-center justify-center border-r-2 border-b-2">
-            <div className="flex items-center mb-2 mt-5">
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-blue-100 mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-blue-500"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
-              </div>
-            </div>
-            <h4 className="text-lg text-center font-bold text-slate-900 p-4">
-              {t("preConstructionPhase")}
-            </h4>
-          </div>
-          <div className="p-4   flex flex-col items-center justify-center border-r-2 border-b-2">
-            <div className="flex items-center mb-2 mt-5">
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-cyan-100 mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-cyan-600"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                  />
-                </svg>
-              </div>
-            </div>
-            <h4 className="text-lg text-center font-bold text-slate-900 p-4">
-              {t("constructionPhase")}
-            </h4>
-          </div>
+      <div className="flex space-x-10 container mx-auto p-10">
+        <article className="w-1/2 relative overflow-hidden rounded-lg shadow transition hover:shadow-lg group hover:cursor-pointer">
+          <Image
+            alt=""
+            src="/assets/imgs/lms.png"
+            width={764}
+            height={510}
+            className="absolute inset-0 h-full w-full object-cover transform transition-transform duration-300 group-hover:scale-125"
+          />
+          <div className="relative bg-gradient-to-t from-gray-900/70 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
+            <div className="p-4 sm:p-6">
+              <Link href="https://ecmi.gov.et" target="_blank">
+                <h3 className="mt-0.5 text-lg text-white">
+                  {t("lms_service")}
+                </h3>
+              </Link>
 
-          <div className="p-4   flex flex-col items-center justify-center  border-b-2">
-            <div className="flex items-center mb-2 mt-5">
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-orange-100 mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-orange-600"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
-                  />
-                </svg>
-              </div>
-            </div>
-
-            <h4 className="text-lg text-center font-bold text-slate-900 p-4">
-              {t("postConstructionPhase")}
-            </h4>
-          </div>
-          <div className="p-4 flex flex-col items-center justify-center border-r-2 border-b-2">
-            <div className="flex items-center mb-2 mt-5">
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-orange-100 mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-yellow-600"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941"
-                  />
-                </svg>
-              </div>
-            </div>
-
-            <h4 className="text-lg text-center font-bold text-slate-900 p-4">
-              {t("contractReviewAndNegotiations")}
-            </h4>
-          </div>
-
-          <div className="p-4   flex flex-col items-center justify-center border-r-2 border-b-2">
-            <div className="flex items-center mb-2 mt-5">
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-purple-100 mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-purple-600"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-                  />
-                </svg>
-              </div>
-            </div>
-
-            <h4 className="text-lg text-center font-bold text-slate-900 p-4">
-              {t("changeOrderAnalysis")}
-            </h4>
-          </div>
-          <div className="p-4   flex flex-col items-center justify-center border-b-2">
-            <div className="flex items-center mb-2 mt-5">
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-orange-100 mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-rose-950"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                  />
-                </svg>
-              </div>
-            </div>
-
-            <h4 className="text-lg text-center font-bold text-slate-900 p-4">
-              {t("constructionDesignReviews")}
-            </h4>
-          </div>
-          <div className="p-4   flex flex-col items-center justify-center border-r-2 ">
-            <div className="flex items-center mb-2 mt-5">
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-blue-100 mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-blue-600"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-                  />
-                </svg>
-              </div>
-            </div>
-
-            <h4 className="text-lg text-center font-bold text-slate-900 p-4">
-              {t("productivityAnalysis")}
-            </h4>
-          </div>
-          <div className="p-4   flex flex-col items-center justify-center border-r-2 ">
-            <div className="flex items-center mb-2 mt-5">
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-lime-100 mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-green-400"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
-                  />
-                </svg>
-              </div>
-            </div>
-
-            <h4 className="text-lg text-center font-bold text-slate-900 p-4">
-              {t("earnedValueManagement")}
-            </h4>
-          </div>
-          <div className="p-4   flex flex-col items-center justify-center ">
-            <div className="flex items-center mb-2 mt-5">
-              <div className="w-12 h-12 flex items-center justify-center rounded-full bg-pink-100 mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-8 h-8 text-pink-600"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                  />
-                </svg>
-              </div>
-            </div>
-
-            <h4 className="text-lg text-center font-bold text-slate-900 p-4">
-              {t("inspectionAndQualityAssurance")}
-            </h4>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-3"></div>
-      </div>
-
-      <div className="flex justify-center  ">
-        <div className="p-4 flex flex-col items-center w-[550px]">
-          <h2 className="text-xl font-bold text-primary-main text-center p-0">
-            Contact Us
-          </h2>
-          <div className="flex justify-center mb-10 w-3/4">
-            <p className="text-sm text-slate-800 text-center">
-              Please fill the below form to get information about our Products
-              or Services
-            </p>
-          </div>
-          <form className="flex flex-col items-center w-full text-sm">
-            <input
-              type="text"
-              placeholder="Company Name"
-              className="w-full p-2  mb-4 border border-gray-300 rounded"
-            />
-            <input
-              type="text"
-              placeholder="Your Full Name"
-              className="w-full p-2  mb-4 border border-gray-300 rounded"
-            />
-            <input
-              type="text"
-              placeholder="Mobile Number"
-              className="w-full p-2 mb-4 border border-gray-300 rounded"
-            />
-            <input
-              type="text"
-              placeholder="Mail Id"
-              className="w-full p-2 mb-4 border border-gray-300 rounded"
-            />
-            <div className="flex justify-start my-10">
-              <p className="text-xs text-slate-800 text-left">
-                We require this information to reach out to you and provide you
-                with information about our products and services. Please rest
-                assured, we prioritize your privacy, and your information is
-                kept completely secure with us. You can review our{" "}
-                <Link href={"#"} className="font-bold hover:underline">
-                  Privacy Policy
-                </Link>{" "}
-                and{" "}
-                <Link href={"#"} className="font-bold hover:underline">
-                  Terms & Conditions
-                </Link>{" "}
-                here.
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
+                {t("lms_service_content")}
               </p>
             </div>
-            <button
-              type="submit"
-              className="bg-primary-main text-white py-2 px-4 rounded hover:bg-primary-dark"
-            >
-              Send Message
-            </button>
-          </form>
-        </div>
+          </div>
+        </article>
+        <article className="w-1/2 relative overflow-hidden rounded-lg shadow transition hover:shadow-lg hover:cursor-pointer group">
+          <Image
+            alt=""
+            src="/assets/imgs/ems.png"
+            width={764}
+            height={510}
+            className="absolute inset-0 h-full w-full object-cover transform transition-transform duration-300 group-hover:scale-125"
+          />
+          <div className=" relative bg-gradient-to-t from-gray-900/70 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
+            <div className="p-4 sm:p-6">
+              <Link href="https://ems.gov.et" target="_blank">
+                <h3 className="mt-0.5 text-lg text-white">
+                  {t("ems_service")}
+                </h3>
+              </Link>
+
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
+                {t("ems_service_content")}
+              </p>
+            </div>
+          </div>
+        </article>
       </div>
     </div>
   );
