6cd52079dd6ade6407e9420167b34d07b2b3e39a
rabra <rabira.hierpa@gmail.com>
2024-11-29 16:36:17 +0300

    refactor: remove unused components and API routes to streamline codebase

--- src/components/safe-html.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import DOMPurify from "dompurify";
-
-const SafeHTML = ({ html }: { html: string }) => {
-  const sanitizedHTML = DOMPurify.sanitize(html);
-  return <div dangerouslySetInnerHTML={{ __html: sanitizedHTML }} />;
-};
-
-export default SafeHTML;
