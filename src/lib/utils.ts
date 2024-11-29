cb48b6a9d5a4b0238d420bd805b5f9ab24347d3c
rabra <rabira.hierpa@gmail.com>
2024-11-29 16:39:02 +0300

    feat: add renameFile utility function to replace spaces in file names

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const renameFile = (file: File) => {
+  const removedSpaces = file.name.replace(/\s/g, "_");
+  return removedSpaces;
+};
