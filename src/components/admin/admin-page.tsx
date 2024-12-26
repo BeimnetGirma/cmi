c52336565c69d190ae3568a76dac65a06015d791
rabra <rabira.hierpa@gmail.com>
2024-12-26 17:27:36 +0300

    chore(admin-page): improve code readability and formatting

--- src/components/admin/admin-page.tsx
+++ src/components/admin/admin-page.tsx
@@ -15,7 +15,12 @@ const AdminPage = ({ ...AdminPageProps }) => {
   return (
     <div className="min-h-screen">
       <div className="w-full">
-        <ImageWithTextOverlay imgUrl="/assets/imgs/header-services.svg" width={1920} height={500} text="Admin Page" />
+        <ImageWithTextOverlay
+          imgUrl="/assets/imgs/header-services.svg"
+          width={1920}
+          height={500}
+          text="Admin Page"
+        />
       </div>
 
       <div className="flex justify-center mt-8 mx-10">
@@ -25,9 +30,12 @@ const AdminPage = ({ ...AdminPageProps }) => {
               key={tab}
               onClick={() => {
                 setComponent(tab.toLowerCase().replace(/\s/g, ""));
-                console.log(component);
               }}
-              className={`text-blue-500 hover:text-blue-700  ${component === tab.toLowerCase().replace(/\s/g, "") ? "font-semibold text-blue-900 " : ""} `}
+              className={`text-blue-500 hover:text-blue-700  ${
+                component === tab.toLowerCase().replace(/\s/g, "")
+                  ? "font-semibold text-blue-900 "
+                  : ""
+              } `}
             >
               {tab}
             </button>
@@ -35,7 +43,13 @@ const AdminPage = ({ ...AdminPageProps }) => {
         </nav>
       </div>
       <div className="border-b-2 border-secondary-400 container mx-auto py-5"></div>
-      <div>{component ? pages[component] : <EditHomePage lng={AdminPageProps.lng} />}</div>
+      <div>
+        {component ? (
+          pages[component]
+        ) : (
+          <EditHomePage lng={AdminPageProps.lng} />
+        )}
+      </div>
     </div>
   );
 };
