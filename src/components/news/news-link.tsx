3916212cc04f664edf0b17aec00e8d493695be8c
rabra <rabira.hierpa@gmail.com>
2025-03-18 13:55:36 +0300

    Update NewsLink component to open Ghost Admin page in a new tab instead of redirecting

--- src/components/news/news-link.tsx
+++ src/components/news/news-link.tsx
@@ -4,8 +4,8 @@ import React, { useEffect } from "react";
 
 const NewsLink = () => {
   useEffect(() => {
-    // Redirect to the Ghost Admin page
-    window.location.assign(`${process.env.NEXT_PUBLIC_GHOST_ADMIN_URL}`);
+    // Redirect to the Ghost Admin page with a new tab
+    window.open(`${process.env.NEXT_PUBLIC_GHOST_ADMIN_URL}`, "_blank");
   }, []);
   return null;
 };
