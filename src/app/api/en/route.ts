5ce4f01ae0b35e00ec751244382599dd95d43f47
Beimnet Girma <beimnetgirma47@gmail.com>
2025-07-31 22:19:41 +0300

    move dynamix translations to db

--- src/app/api/en/route.ts
+++ src/app/api/en/route.ts
@@ -1,20 +1,27 @@
 import fs from "fs";
 import path from "path";
+import prisma from "@/db";
 // const filePath = path.resolve("src/app/i18n/locales/en/translation.json");
 const filePath = path.join(process.cwd(), "src/app/i18n/locales/en/translation.json");
-console.log("***************************************************************************");
-
-console.log("Getting translation file from:", filePath);
 
 export async function GET() {
-  const jsonData = fs.readFileSync(filePath);
-  const data = JSON.parse(jsonData.toString());
-  return Response.json(data);
+  const translations = await prisma.translation.findMany({
+    where: { language: "en" },
+  });
+  const json = Object.fromEntries(translations.map((t: { key: any; value: any }) => [t.key, t.value]));
+  return Response.json(json);
 }
 
 export async function POST(req: Request) {
   const data = await req.json();
-  fs.writeFileSync(filePath, JSON.stringify(data));
+  const entries = Object.entries(data);
+  for (const [key, value] of entries) {
+    await prisma.translation.upsert({
+      where: { key_language: { key, language: "en" } },
+      update: { value: value as any },
+      create: { key, language: "en", value: value as any },
+    });
+  }
 
-  return Response.json({ message: "JSON file has been updated successfully" });
+  return Response.json({ message: "Translations updated." });
 }
